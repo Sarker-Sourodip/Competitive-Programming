7385f37f1c89f5c8c35f1c86fd3e3b9008e0c7d9
Sarker-Sourodip <sourodipsarker@gmail.com>
2026-08-30 04:54:08 +0600

    Solved the 4A problem of Codeforces with C++.

---
+++ C++/4A.cpp
@@ -0,0 +1,19 @@
+// Coded By Sourodip Sarker
+# include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(int argc, char* argv[])
+{
+    int num;
+    cin >> num;
+    if(num%2 == 0 && num > 2)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
