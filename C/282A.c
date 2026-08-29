3238c9f80e9721a74f1c10df257ad84f426eca64
Sarker-Sourodip <sourodipsarker@gmail.com>
2026-08-30 05:01:47 +0600

    Solved the 282A problem of Codeforces with C.

---
+++ C/282A.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+    int n, x = 0;
+    scanf("%d", &n);
+    char ch[4];
+ 
+    for (int i = 0; i < n; i++) {
+        scanf("%s", ch);
+        if (ch[0] == '+' || ch[1] == '+') {
+            x++;
+        } else {
+            x--;
+        }
+    }
+ 
+    printf("%d\n", x);
+    return 0;
+}
